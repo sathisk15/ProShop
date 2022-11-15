9c8aa9a110bd849674d168e6bc694550df533c3d
Sathis <rsathisk15@gmail.com>
2022-11-15 17:59:10 +0100

    Product screen setup done

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+    const {id} = useParams()
+    const product = products.find((p)=>p._id === id)
   return (
-    <div>ProductScreen</div>
+    <>
+    <Link className="btn btn-light my-3" to='/'>Go Back</Link>
+    <Row>
+        <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3} variant="flush">
+            <ListGroup.Item>
+                <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                Price: ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+                Description: ${product.description}
+            </ListGroup.Item>
+        </Col>
+        <Col md={3}>
+            <Card>
+                <ListGroup.Item>
+                    <Row className="py-2 px-2">
+                        <Col>Price:</Col>
+                        <Col><strong>${product.price}</strong></Col>
+                    </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Row className="py-2 px-2">
+                        <Col>Status:</Col>
+                        <Col>{product.countInStock > 0 ? 'In Stock':'Out of Stock'}</Col>
+                    </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className='mx-auto pb-2'>
+                    <Button className="btn-block" type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
+                </ListGroup.Item>
+            </Card>
+        </Col>
+    </Row>
+    </>
   )
 }
 
