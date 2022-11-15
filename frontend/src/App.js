ebc67428c1cf0d4f9c0eefaa23b0288f7be79b0e
Sathis <rsathisk15@gmail.com>
2022-11-15 17:17:37 +0100

    react-router-dom implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import {Container} from 'react-bootstrap'
-import HomeScreen from './screens/HomeScreen'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-        <HomeScreen />
-      </Container>
-    </main>
-    <Footer />
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,33 @@
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-            <Navbar.Brand href='/'>ProShop</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav' className='nav-bar'>
-              <Nav className='ml-auto'>
-                <Nav.Link href='/cart'>
-                  <i className='fas fa-shopping-cart'></i> Cart
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="nav-bar">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
                 </Nav.Link>
-                <Nav.Link href='/login'>
-                  <i className='fas fa-user'></i> Sign In
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Sign In
                 </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
