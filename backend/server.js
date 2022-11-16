b6a39c291b32d2ba977d1be56f222c6938b98bc0
Sathis <rsathisk15@gmail.com>
2022-11-16 23:58:49 +0100

    Environmnet varialbe setup with dotenv package, DB setup with mongoose package

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 // const express = require('express')
 import express from 'express'
+import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
+// It is for Environment Variable setup as process.env.<Variable name declared in the .env file>
+dotenv.config()
+// Routing setup
 const app = express()
-
+// To Connect DB setup
+connectDB()
 app.get('/',(req, res)=>{
     res.send('Api is running')
 })
@@ -17,4 +23,6 @@ app.get('/api/products/:id', (req,res)=>{
     res.send(product)
 })
 
-app.listen(5000, console.log("Backend server running port 5000"))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Backend server running in ${process.env.Node_ENV} mode on port ${PORT}`))
