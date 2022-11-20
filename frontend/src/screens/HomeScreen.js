aa9e46d1f4231c5a921c3d1676be91aff24f1b34
Sathis <rsathisk15@gmail.com>
2022-11-20 10:45:37 +0100

    Product detatils screen and its reducers setup done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,38 @@
-import React,{useEffect} from 'react'
-import {Row, Col} from 'react-bootstrap'
-import Product from '../components/Product'
-import { useDispatch, useSelector } from 'react-redux'
-import { listProducts } from '../actions/productActions'
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const dispatch =  useDispatch()
-  const productList = useSelector(state=>state.productList)
-  const {loading, error, products} = productList
-  
-  useEffect(()=>{
-    dispatch(listProducts())
-  }, [dispatch])
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
-        <h1>Latest Products</h1>
+      <h1>Latest Products</h1>
+      {loading ? (
+          <Loader />
+      ) : error ? (
+          <Message variant="danger" children={error}/>
+      ) : (
         <Row>
-            {products.map((product, idx)=>(
-                <Col key={idx} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-            ))}
+          {products.map((product, idx) => (
+            <Col key={idx} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
         </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,62 +1,81 @@
-import { Link, useParams } from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
-import Rating from '../components/Rating'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { productDetails } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
-    const {id} = useParams()
-    const [product, setProduct] = useState({})
-    useEffect(()=>{
-        const fetchProduct = async ()=>{
-            const {data} = await axios.get(`/api/products/${id}`)
-            setProduct(data)
-        }
-        fetchProduct()
-    }, [id])
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const products = useSelector((state) => state.productDetails);
+  const { product, loading, error } = products;
+
+  useEffect(() => {
+    dispatch(productDetails(id));
+  }, [dispatch, id]);
   return (
     <>
-    <Link className="btn btn-light my-3" to='/'>Go Back</Link>
-    <Row>
-        <Col md={6}>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger" children={error} />
+      ) : (
+        <Row>
+          <Col md={6}>
             <Image src={product.image} alt={product.name} fluid></Image>
-        </Col>
-        <Col md={3} variant="flush">
+          </Col>
+          <Col md={3} variant="flush">
             <ListGroup.Item>
-                <h3>{product.name}</h3>
+              <h3>{product.name}</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
             </ListGroup.Item>
-            <ListGroup.Item>
-                Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-                Description: {product.description}
-            </ListGroup.Item>
-        </Col>
-        <Col md={3}>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </Col>
+          <Col md={3}>
             <Card>
-                <ListGroup.Item>
-                    <Row className="py-2 px-2">
-                        <Col>Price:</Col>
-                        <Col><strong>${product.price}</strong></Col>
-                    </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Row className="py-2 px-2">
-                        <Col>Status:</Col>
-                        <Col>{product.countInStock > 0 ? 'In Stock':'Out of Stock'}</Col>
-                    </Row>
-                </ListGroup.Item>
-                <ListGroup.Item className='mx-auto pb-2'>
-                    <Button className="btn-block" type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
-                </ListGroup.Item>
+              <ListGroup.Item>
+                <Row className="py-2 px-2">
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row className="py-2 px-2">
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className="mx-auto pb-2">
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
             </Card>
-        </Col>
-    </Row>
+          </Col>
+        </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
