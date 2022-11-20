b208492d7022d70f8c2ce48ff8f77d3e8eaeb705
Sathis <rsathisk15@gmail.com>
2022-11-20 10:45:37 +0100

    Product detatils screen and its reducers setup done

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
 import axios from "axios";
@@ -11,7 +14,7 @@ export const listProducts = () => async (dispatch) => {
     const { data } = await axios.get("/api/products");
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
-      payload: data
+      payload: data,
     });
   } catch (error) {
     dispatch({
@@ -20,6 +23,19 @@ export const listProducts = () => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.messsage
           : error.message,
+    });
+  }
+};
+
+export const productDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.message,
     });
   }
 };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,11 @@
+import { STATES } from "mongoose";
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -16,3 +20,17 @@ export const productListReducer = (state = { products: [] }, action) => {
       return state;
   }
 };
+
+
+export const productDetailsReducer = (state = {product: {}}, action) =>{
+  switch (action.type){
+    case PRODUCT_DETAILS_REQUEST: 
+      return {loading: true, ...STATES}
+    case PRODUCT_DETAILS_SUCCESS:
+      return {loading: false, product: action.payload}
+    case PRODUCT_DETAILS_FAIL:
+      return {loading: false, error: action.payload}
+    default:
+      return state
+      }
+  }
