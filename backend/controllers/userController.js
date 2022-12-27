f062af8860fb6e1edeecafc006ff3c075a9067f4
Sathis <rsathisk15@gmail.com>
2022-12-27 17:05:12 +0100

    user Auth login/logout and Register funtionalities added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
-
+import generateToken from '../utils/generateToken.js';
+import bcrypt from 'bcryptjs';
 /// @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -23,4 +24,51 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+/// @desc    Register usre
+// @route   POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  let { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  // Encode the Password
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+/// @desc    Get user profile
+// @route   POST /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('user not found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,26 +8,34 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItems = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfo = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: { cartItems },
+  userLogin: { userInfo },
 };
 
 const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
