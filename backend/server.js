92a3649a39f96c84d856deedaafe954fe7b1e77d
Sathis <rsathisk15@gmail.com>
2024-03-05 21:44:47 +0530

    Removed console.log

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ var whitelist = [
 
 var corsOptions = {
   origin: function (origin, callback) {
-    console.log(origin)
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
