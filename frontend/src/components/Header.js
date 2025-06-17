5fbe24b726fa0c22ec7189c9a43091d5bba0d74a
Sathis <rsathisk15@gmail.com>
2025-06-17 15:22:22 +0200

    Badge added for Cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,24 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../actions/userActions';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.userLogin);
+  const { cartItems } = useSelector((state) => state.cart);
+  const [numberOfItemsInTheCart, setNumberOfItemsInTheCart] = useState(0);
   const logoutHandler = () => {
     dispatch(logout());
   };
+
+  useEffect(() => {
+    const totalItems = cartItems.reduce(
+      (accumulator, item) => accumulator + Number(item.qty),
+      0
+    );
+    setNumberOfItemsInTheCart(totalItems);
+  }, [cartItems]);
   return (
     <header>
       <Navbar bg="black" variant="dark" expand="lg" collapseOnSelect>
@@ -20,8 +31,14 @@ const Header = () => {
           <Navbar.Collapse id="basic-navbar-nav" className="nav-bar">
             <Nav className="ml-auto">
               <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
+                <Nav.Link className="position-relative me-4">
+                  Cart{' '}
+                  {numberOfItemsInTheCart > 0 && (
+                    <span class="badge position-absolute top-0 start-100 translate-middle bg-black border border-light rounded-circle">
+                      {numberOfItemsInTheCart}
+                    </span>
+                  )}
+                  <i className="fas fa-shopping-cart"></i>
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
